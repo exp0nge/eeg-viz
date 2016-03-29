c2207a3dc2c7a07b5ae971b9e4cd7e437b5fb6cc
Fioger <fioger95@Live.com>
2016-03-29 14:57:40 -0400

    Plotted each channel with all of its data points

---
+++ eeg_data_plot.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+from scipy.io import loadmat
+
+m = loadmat('s5d2nap_justdata.mat')
+
+matrix = m['s5d2nap']
+
+s = pd.DataFrame(matrix).transpose()
+
+for i in range(64):
+    print "Plotting Channel %s" % i
+    plt.plot(range(len(s[i])), s[i])
+    plt.title('Channel %s' % i)
+    plt.xlabel('Time (ms)')
+    plt.ylabel('Value')
+    plt.savefig('Channel %s eeg data.svg' % i)
+    plt.show()
