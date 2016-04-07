19216e4df3c90f88f52a56df248570904a92c80c
Enan <enan7890@gmail.com>
2016-04-06 23:04:34 -0400

    Added bandpass and commented out other filters

--- graphing_matrix.py
+++ graphing_matrix.py
@@ -19,46 +19,58 @@ m = loadmat('s5d2nap_justdata.mat')
 matrix = m['s5d2nap']
 
 #lowpass
-b1, a1 = signal.butter(3, 0.1, 'low', analog=True)
-w1, h1 = signal.freqs(b1, a1)
+#b1, a1 = signal.butter(2, 0.1, 'low', analog=True)
+#w1, h1 = signal.freqs(b1, a1)
 
 #highpass
-b2, a2 = signal.butter(2, 0.1, 'high', analog=True)
-w2, h2 = signal.freqs(b2, a2)
+#b2, a2 = signal.butter(2, 0.5, 'high', analog=True)
+#w2, h2 = signal.freqs(b2, a2)
+
+#bandpass
+b3, a3 = signal.butter(2, [0.1,0.5], 'band', analog=True)
+w3, h3 = signal.freqs(b3, a3)
 
 #create matrix
 new_matrix = []
+
 for index, row in (enumerate(matrix)):
-	new_matrix.append(row[0:1000])
+	new_matrix.append(row[0:600000])
 # print len(new_matrix[0])
 eeg = np.array(new_matrix)
 
 
 #normal print
-mat = []
-for row in range(len(new_matrix)):
-	mat.append(eeg[row])
+#mat = []
+#for row in range(len(new_matrix)):
+#	mat.append(eeg[row])
 
-s = pd.DataFrame(mat).transpose()
+#s = pd.DataFrame(mat).transpose()
 
 #lowpass print
-mat1 = []
-for row in range(len(new_matrix)):
-	mat1.append(signal.lfilter(b1, a1, eeg[row]))
+#mat1 = []
+#for row in range(len(new_matrix)):
+#	mat1.append(signal.lfilter(b1, a1, eeg[row]))
 
-s1 = pd.DataFrame(mat1).transpose()
+#s1 = pd.DataFrame(mat1).transpose()
 
 #highpass print
-mat2 = []
+#mat2 = []
+#for row in range(len(new_matrix)):
+#	mat2.append(signal.lfilter(b2, a2, eeg[row]))
+#s2 = pd.DataFrame(mat2).transpose()
+
+mat3 = []
 for row in range(len(new_matrix)):
-	mat2.append(signal.lfilter(b2, a2, eeg[row]))
-s2 = pd.DataFrame(mat2).transpose()
+	mat3.append(signal.lfilter(b3, a3, eeg[row]))
+
+s3 = pd.DataFrame(mat3).transpose()
 
 # In[ ]:
 #s.plot(legend=False)
-s1.plot(legend=False)
+#s1.plot(legend=False)
 #s2.plot(legend=False)
-#s.legend_.remove()
+s3.plot(legend=False)
+
 # cumsum() adds the value of each channel and displays the sum
 # s = s.cumsum()
 # s.plot()
