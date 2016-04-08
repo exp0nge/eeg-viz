14b39523325d4d73cbd642eff12db57a9c00e666
Fioger <fioger95@Live.com>
2016-04-08 16:20:57 -0400

    Added Sorted/Unsorted graphs of regular and absolute value of comparisons/edges

---
+++ sorted_z_score_pearson_graph.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.io import loadmat
+import numpy as np
+from scipy.stats import pearsonr
+import cPickle
+
+# load np array
+pearson_z_score = np.load('z_score_pearson_dump.dump')
+
+# absolute value
+for i in range(len(pearson_z_score)):
+    pearson_z_score[i] = abs(pearson_z_score[i])
+
+# sort data
+sorted_pearson = sorted(pearson_z_score)
+
+# plot data
+plt.bar(range(len(sorted_pearson)), sorted_pearson)
+plt.title('Absolute Value Correlations between 2 Channels')
+plt.xlim(-1, 2017)
+plt.ylim(0, 1.1)
+plt.xlabel('Channel vs Channel')
+plt.ylabel('Correlation value')
+# plt.xticks(range(64), labels)
+plt.savefig('64_absolute_value_z_score_pearson.svg')
+plt.show()
